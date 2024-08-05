e2b76c9b5736ea3f9f7f63dcc8a49b396a5389d7
MichaelDasen <michael.hyun.dasen@gmail.com>
2024-08-05 16:00:53 -0400

    Mining Stuff

---
+++ QuarryCrawl/Assets/Scripts/CrystalScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalScript : MonoBehaviour
+{
+    public GameObject player;
+    public float holdTime = 0f;
+    public float miningSpeed = 1f;
+    public float mineTime = 3f;
+    public bool keyHeld = false;
+    public bool inRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (keyHeld && Vector3.Distance(transform.position, player.transform.position) < 2)
+        {
+            holdTime += Time.deltaTime;
+        }
+        else
+        {
+            holdTime = 0;
+            keyHeld = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            keyHeld = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            keyHeld = false; ;
+        }
+        if (holdTime > mineTime)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+    }
+}
