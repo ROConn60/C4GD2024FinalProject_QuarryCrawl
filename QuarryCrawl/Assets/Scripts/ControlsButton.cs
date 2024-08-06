e175992e4c17aa7cbf186bd956aef88a5f97e2d3
n0raaa1 <nw720159@gmail.com>
2024-08-06 00:09:36 -0400

    controls

--- QuarryCrawl/Assets/Scripts/ControlsButton.cs
+++ QuarryCrawl/Assets/Scripts/ControlsButton.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ControlsButton : MonoBehaviour
+public class ControlsButton : MonoBehaviour 
 {
-    // Start is called before the first frame update
+    public GameObject StartScreen;
+    public GameObject ControlsScreen;
+
+    private Button controls;
     void Start()
     {
-        
+        controls = GetComponent<Button>();
+        controls.onClick.AddListener(ShowControls);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowControls()
     {
         
+        StartScreen.SetActive(false);
+        ControlsScreen.SetActive(true);
     }
 }
+
+
