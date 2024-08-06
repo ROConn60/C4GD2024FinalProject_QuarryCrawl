4d2bec2a9bde44397fe4114a2b9007f0805c60d6
MichaelDasen <michael.hyun.dasen@gmail.com>
2024-08-06 14:41:22 -0400

    Flashlight power

--- QuarryCrawl/Assets/Scripts/Batteries.cs
+++ QuarryCrawl/Assets/Scripts/Batteries.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Batteries : MonoBehaviour
+public class Batteries : MonoBehaviour, IInteractable
 {
+    public GameObject flashlight;
+
+    public void Interact()
+    {
+        flashlight.GetComponent<FlashlightScript>().power = 100;
+        Debug.Log("Power reset");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
